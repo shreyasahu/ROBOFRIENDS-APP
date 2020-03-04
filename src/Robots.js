fb87dd9eb6399ef36d5f5a4d9aa9fdbc9ab29e7e
unknown <comviva@MCKD-TRNG30-07.COMVIVA.COM>
2020-03-04 16:28:50 +0530

    second

---
+++ src/Robots.js
@@ -0,0 +1,26 @@
+export const Robots = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'sincere@april.biz'
+    },
+  
+    {
+      id: 2,
+      name: 'Ranit Howel',
+      username: 'sasade@april.biz'
+    },
+  
+    {
+      id: 3,
+      name: 'patigy marshal',
+      username: 'sdfdg@april.biz'
+    },
+  
+    {
+      id: 4,
+      name: 'huwai juhu',
+      username: 'sibcb@april.biz'
+    },
+  
+   ]
