d0df8e1db1ac256c94ccd964686c41768e302a3e
unknown <comviva@MCKD-TRNG30-07.COMVIVA.COM>
2020-03-04 17:09:24 +0530

    successful

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, {component} from 'react'
 import CardList from './CardList'
 import {Robots} from './Robots'
 import SearchBox from './SearchBox'
+import { render } from '@testing-library/react'
+ 
 
-const App = () => {
-    return  (
-        <div className='tc'>
-        <h1>ROBOFRIENDS</h1>
-        <SearchBox/>
-        <CardList Robots= {Robots}/>
-        </div>
-    );
+
+ class App extends component {
+     constructor() {
+        
+         super()
+         this.state = {
+              Robots: Robots,
+              searchfield:''
+         }
+     }
+     
+    render()
+    {
+        return  (
+            <div className='tc'>
+            <h1>ROBOFRIENDS</h1>
+            <SearchBox/>
+            <CardList Robots= {this.state.Robots}/>
+            </div>
+        );
+    }
+   
 }
 
-export default App
+export default App;
